b1794b9ec99c30d0135e14c0c6f64b14694ce289
Barkin Monster <barkinkaplan@gmail.com>
2018-04-12 20:17:44 +0300

    correction in menu.php

--- menu.php
+++ menu.php
@@ -10,14 +10,14 @@
 
 <ul type="square">
   <div class="menu">
-  <li><a href="list_all_members">List Members</a></li>
-  <li><a href="member_registration_panel">Add Member</a></li>
-  <li><a href="list_all_courses">List Courses</a></li>
-  <li><a href="course_creation_panel">Add Course</a></li>
-  <li><a href="list_all_boats">List Boats</a></li>
-      <li><a href="add_new_boat_panel">Add New Boat</a></li>
-      <li><a href="AddEventPanel">Add New Event</a></li>
-      <li><a href="AddItemPanel">Add Item</a></li>
+  <li><a href="list_all_members.php">List Members</a></li>
+  <li><a href="member_registration_panel.php">Add Member</a></li>
+  <li><a href="list_all_courses.php">List Courses</a></li>
+  <li><a href="course_creation_panel.php">Add Course</a></li>
+  <li><a href="list_all_boats.php">List Boats</a></li>
+      <li><a href="add_new_boat_panel.php">Add New Boat</a></li>
+      <li><a href="AddEventPanel.php">Add New Event</a></li>
+      <li><a href="AddItemPanel.php">Add Item</a></li>
       <li><a href="CommitteeCreationPanel1.php">Add Committee</a></li>
 </ul>
 </body>
